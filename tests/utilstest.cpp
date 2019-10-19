86bd470f6453643376807c51a1b9dea272ba091a
sabi1k98 <rsabol1k98@gmail.com>
2019-10-19 17:03:00 +0200

    1 more test case

--- tests/utilstest.cpp
+++ tests/utilstest.cpp
@@ -3,7 +3,18 @@
 #include "dictionary.hpp"
 
 
-TEST_CASE("DICTIONARY-INVALIDPATH-THROWS") {
+TEST_CASE("BIP39::Dictionary - invalid path") {
     const std::string path("/path/that/hopefully/doesnt/exist");
     REQUIRE_THROWS_AS(BIP39::Dictionary(path), std::invalid_argument);
+}
+
+TEST_CASE("BIP39::Dictionary - ASCII files") {
+    BIP39::Dictionary dict("wordlists/english.txt");
+    SECTION("Index to word and back") {
+        for (int i = 0; i < 2048; i++) {
+            std::wstring current;
+            CHECK_NOTHROW(current = dict.getWord(i));
+            REQUIRE(dict.getIndex(current) == i);
+        }
+    }
 }
