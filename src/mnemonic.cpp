7d3486d267b52a9c26f12b75c491d2f84f215d78
Gariane <dazdnik11@outlook.com>
2019-10-21 13:23:10 +0200

    Add empty implementation

--- src/mnemonic.cpp
+++ src/mnemonic.cpp
@@ -11,19 +11,19 @@ Mnemonic::Mnemonic(const std::vector<std::string>& phrase, const BIP39::Dictiona
 }
 
 Mnemonic::binData Mnemonic::getEntropy() const {
-
+    return {};
 }
 
 std::vector<std::string> Mnemonic::getPhrase() const {
-
+    return {};
 }
 
 std::string Mnemonic::getSeed() const {
-
+    return "";
 }
 
 bool Mnemonic::checkPhraseSeedPair(const std::vector<std::string>& phrase, const std::string& seed, const BIP39::Dictionary& dictionaryPath) {
-
+    return true;
 }
 
 } // end namespace BIP39
